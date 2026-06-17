66716aa319b2a8315d4e9de6b5f8334390b241c8
Armontex <windle1337@gmail.com>
2026-06-17 15:40:16 +0500

    feat: add app

---
+++ backend/src/roomslot/bootstrap/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from roomslot.bootstrap.lifespan import lifespan
+from roomslot.config.settings import get_settings
+from roomslot.containers.container import Container
+from roomslot.observation.logs.setup import setup_logging
+
+
+def create_app() -> FastAPI:
+    settings = get_settings()
+
+    setup_logging(settings.logging)
+
+    container = Container(settings=settings)
+
+    app = FastAPI(
+        debug=False,  # TODO: вынести в настройки
+        lifespan=lifespan,
+    )
+
+    app.state.container = container
+
+    return app
