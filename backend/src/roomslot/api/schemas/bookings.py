25cff6c2f0271777e69b7da31e104d35fa88614e
Armontex <windle1337@gmail.com>
2026-06-18 23:04:47 +0500

    feat: add CreateBookingRequest validations

--- backend/src/roomslot/api/schemas/bookings.py
+++ backend/src/roomslot/api/schemas/bookings.py
@@ -1,5 +1,9 @@
-from datetime import date, datetime, time
+from calendar import Day
+from datetime import date as date_
+from datetime import datetime, time
 from uuid import UUID
+
+from pydantic import field_validator
 
 from roomslot.api.schemas.base import BaseResponse, BaseSchema, PaginatedResponse
 from roomslot.domain.enums import BookingStatus
@@ -7,22 +11,40 @@ from roomslot.domain.enums import BookingStatus
 
 class CreateBookingRequest(BaseSchema):
     room_id: UUID
-    date: date
+    date: date_
     start_time: time
+
+    @field_validator("date")
+    @classmethod
+    def validate_date(cls, value: date_) -> date_:
+        if value.weekday() > Day.FRIDAY:
+            raise ValueError("The slot booking date must be between Monday and Friday.")
+        return value
+
+    @field_validator("start_time")
+    @classmethod
+    def validate_start_time(cls, value: time) -> time:
+        if value.minute != 0 or value.second != 0 or value.microsecond != 0:
+            raise ValueError("Slot start time must be a full hour")
+
+        if value.hour not in range(10, 19):
+            raise ValueError("Slot start time must be between 10:00 and 18:00")
+
+        return value
 
 
 class CreateBookingResponse(BaseResponse):
     id: UUID
     room_id: UUID
     user_id: UUID
-    date: date
+    date: date_
     start_time: time
     status: BookingStatus
     created_at: datetime
 
 
 class BookingSlot(BaseSchema):
-    date: date
+    date: date_
     start_time: time
 
 
